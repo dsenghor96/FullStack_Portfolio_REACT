8a995b27cd9a47607b1cc8e7136944a7e8a39833
Dieyna <dsenghor96@gmail.com>
2026-04-18 19:38:53 +0000

    Composant principal de l'application

--- src/App.jsx
+++ src/App.jsx
@@ -2,3 +2,5 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Dossier from './components/Dossier'
 import DetaillerProjet from './components/DetaillerProjet'
 import './App.css'
+
+function App() { }
