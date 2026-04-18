cbea26db4651cb0d64446fd5ff1fbe47671012fc
Dieyna <dsenghor96@gmail.com>
2026-04-18 19:36:22 +0000

    import du css

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Dossier from './components/Dossier'
 import DetaillerProjet from './components/DetaillerProjet'
+import './App.css'
