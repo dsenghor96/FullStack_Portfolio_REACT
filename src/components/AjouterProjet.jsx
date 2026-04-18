1dab467595ad785ac6536ad6c34b5483c37ac8af
Dieyna <dsenghor96@gmail.com>
2026-04-18 20:10:02 +0000

    creation de la Fonction appelée à la soumission du formulaire

--- src/components/AjouterProjet.jsx
+++ src/components/AjouterProjet.jsx
@@ -6,4 +6,20 @@ function AjouterProjet({ onAjouter }) {
     const [description, setDescription] = useState('')
     const [technologie, setTechnologie] = useState('')
     const [lien, setLien] = useState('')
+
+    function handleSubmit(e) {
+    e.preventDefault()
+
+    if (!libelle) return
+
+    const nouveauProjet = { libelle, image, description, technologie, lien }
+
+    onAjouter(nouveauProjet)
+
+    setLibelle('')
+    setImage('')
+    setDescription('')
+    setTechnologie('')
+    setLien('')
+  }
 }
