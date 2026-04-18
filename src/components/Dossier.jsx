67fef73c3d886eef9d069f2a15df5a6a942055a4
Dieyna <dsenghor96@gmail.com>
2026-04-18 19:52:41 +0000

    gestion du stockage de la liste des projets et de l'affichage avec usestate

--- src/components/Dossier.jsx
+++ src/components/Dossier.jsx
@@ -1,3 +1,9 @@
 import { useState, useEffect } from 'react'
 import Projet from './Projet'
 import AjouterProjet from './AjouterProjet'
+
+function Dossier() { 
+    const [projets, setProjets] = useState([])
+    const [afficherFormulaire, setAfficherFormulaire] = useState(false)
+
+}
