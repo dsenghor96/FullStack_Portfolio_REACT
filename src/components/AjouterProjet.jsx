49fd8ddfb044410b9510b15647ebe4f054fa730a
Dieyna <dsenghor96@gmail.com>
2026-04-18 20:06:35 +0000

    gestion de l'état de chaque champ du formulaire

--- src/components/AjouterProjet.jsx
+++ src/components/AjouterProjet.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 
 function AjouterProjet({ onAjouter }) {
-    
+    const [libelle, setLibelle] = useState('')
+    const [image, setImage] = useState('')
+    const [description, setDescription] = useState('')
+    const [technologie, setTechnologie] = useState('')
+    const [lien, setLien] = useState('')
 }
